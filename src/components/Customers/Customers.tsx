8807d2cad3ab76ca3406757e6247bb99e9000157
Vladimir Peretiatko <djbob2000@gmail.com>
2023-07-10 16:14:23 +0300

    add customers

--- src/components/Customers/Customers.tsx
+++ src/components/Customers/Customers.tsx
@@ -106,5 +106,92 @@ export const Customers: React.FC = () => {
     return () => clearTimeout(delay);
   }, [searchValue, customersData]);
 
-  return <></>;
+  return (
+    <>
+      <main className={css.main}>
+        <h2 className={css.main__login}>Hello Evano 👋🏼,</h2>
+        <section className={css.customers}>
+          <div className={css.customers__header}>
+            <div className={css['customers__header-info']}>
+              <div className={css.customers__title}>All Customers</div>
+              <div className={css.customers__subtitle}>Active Members</div>
+            </div>
+            <div className={css.customers__search}>
+              <svg className={css['customers__search-icon']}>
+                <use href={`${sprite}#search`}></use>
+              </svg>
+              <input type="text" placeholder="Search" onChange={handleSearchChange} />
+            </div>
+          </div>
+
+          <div className={css.table}>
+            <div className={css.table__inner}>
+              <div className={`${css.table__header} ${css.table__row}`}>
+                <div className={css['table__header-item']}>Customer Name</div>
+                <div className={css['table__header-item']}>Company</div>
+                <div className={css['table__header-item']}>Phone Number</div>
+                <div className={css['table__header-item']}>Email</div>
+                <div className={css['table__header-item']}>Country</div>
+                <div className={css['table__header-item']}>Status</div>
+              </div>
+              <div className={css.table__customers}>
+                {filteredCustomers.map((customer) => (
+                  <div className={`${css.table__customer} ${css.table__row}`} key={customer.id}>
+                    <div className={css.table__item}>{customer.name}</div>
+                    <div className={css.table__item}>{customer.company}</div>
+                    <div className={css.table__item}>{customer.phone}</div>
+                    <div className={css.table__item}>{customer.email}</div>
+                    <div className={css.table__item}>{customer.country}</div>
+                    <div className={css.table__item}>
+                      <div
+                        className={`${css.table__status} ${
+                          customer.status === 'Active' ? css.active : css.inactive
+                        }`}
+                      >
+                        {customer.status}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className={css['customers-pagination']}>
+            <div className={css['customers-pagination__info']}>
+              Showing data 1 to 8 of 256K entries
+            </div>
+            <ul className={`${css['customers-pagination__numbers']} ${css['pagination-list']}`}>
+              <li className={css['pagination-list__item']}>
+                <a href="#prev">&#10094;</a>
+              </li>
+              <li className={`${css['pagination-list__item']} ${css['pagination-item__active']}`}>
+                <a href="#1" className={css.active}>
+                  1
+                </a>
+              </li>
+              <li className={css['pagination-list__item']}>
+                <a href="#2">2</a>
+              </li>
+              <li className={css['pagination-list__item']}>
+                <a href="#3">3</a>
+              </li>
+              <li className={css['pagination-list__item']}>
+                <a href="#4">4</a>
+              </li>
+              <li className={css['pagination-list__dots']}>
+                <a href="#...">...</a>
+              </li>
+              <li className={css['pagination-list__item']}>
+                <a href="#40">40</a>
+              </li>
+              <li className={css['pagination-list__item']}>
+                <a href="#next">&#10095;</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
